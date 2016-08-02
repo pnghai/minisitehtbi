175a479c61211482119c4c2503cc77c165484fce
Hai Phan Nguyen <pnghai@gmail.com>
2016-08-02 17:50:12 +0700

    add carousel and fancybox support

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,4 +3,19 @@ import App from './app';
 
 var $ = require('jquery');
 
-console.log('JS initialized in version:', App.version);
+//import $ from 'jquery';
+$.noConflict();
+jQuery( document ).ready(function( $ ) {
+    // Code that uses jQuery's $ can follow here.
+    $('.b-testimonials .owl-carousel').owlCarousel({
+        autoPlay:7000,
+        items:1,
+        margin:0,
+        singleItem:true,
+        navigation:true,
+        navigationText : ["&laquo;","&raquo;"],
+        pagination: true,
+        stopOnHover: true
+    });
+    $(".fancybox").fancybox();
+});
